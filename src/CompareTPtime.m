6e6f015dd3576c29edabbcf47bf7274a8366d757
LonnekeRoelofs <lonneke.roelofs@live.nl>
2020-10-02 11:32:23 +0200

    Adjusted to run function

--- src/CompareTPtime.m
+++ src/CompareTPtime.m
@@ -10,31 +10,22 @@ experiment  = 'Exp006';
 [TP006 timeseries006] = LoadingTPdata(experiment);
 
 % Finding tidal prism at different locations along flume for all time steps
-TP006_w4    = [TP006_00291(104) TP006_00771(104) TP006_02281(104) TP006_04401(104) TP006_05887(104) TP006_06884(104) TP006_07869(104) TP006_08863(104) TP006_10915(104) TP006_13076(104)];
-TP006_w6    = [TP006_00291(184) TP006_00771(184) TP006_02281(184) TP006_04401(184) TP006_05887(184) TP006_06884(184) TP006_07869(184) TP006_08863(184) TP006_10915(184) TP006_13076(184)];
-TP006_w8    = [TP006_00291(264) TP006_00771(264) TP006_02281(264) TP006_04401(264) TP006_05887(264) TP006_06884(264) TP006_07869(264) TP006_08863(264) TP006_10915(264) TP006_13076(264)];
-TP006_w12   = [TP006_00291(424) TP006_00771(424) TP006_02281(424) TP006_04401(424) TP006_05887(424) TP006_06884(424) TP006_07869(424) TP006_08863(424) TP006_10915(424) TP006_13076(424)];
-TP006_w16   = [TP006_00291(584) TP006_00771(584) TP006_02281(584) TP006_04401(584) TP006_05887(584) TP006_06884(584) TP006_07869(584) TP006_08863(584) TP006_10915(584) TP006_13076(584)];
+TP006_w4    = [TP006(1,104) TP006(2,104) TP006(3,104) TP006(4,104) TP006(5,104) TP006(6,104) TP006(7,104) TP006(8,104) TP006(9,104) TP006(10,104)];
+TP006_w6    = [TP006(1,184) TP006(2,184) TP006(3,184) TP006(4,184) TP006(5,184) TP006(6,184) TP006(7,184) TP006(8,184) TP006(9,184) TP006(10,184)];
+TP006_w8    = [TP006(1,264) TP006(2,264) TP006(3,264) TP006(4,264) TP006(5,264) TP006(6,264) TP006(7,264) TP006(8,264) TP006(9,264) TP006(10,264)];
+TP006_w12   = [TP006(1,424) TP006(2,424) TP006(3,424) TP006(4,424) TP006(5,424) TP006(6,424) TP006(7,424) TP006(8,424) TP006(9,424) TP006(10,424)];
+TP006_w16   = [TP006(1,584) TP006(2,584) TP006(3,584) TP006(4,584) TP006(5,584) TP006(6,584) TP006(7,584) TP006(8,584) TP006(9,584) TP006(10,584)];
 
-%getting X matrix for plotting
-load(['..\data\processed\',experiment,'\cycle',num2str(timeseries006(end)),'\X_array.ASC']);
-maxJ = 118;
-maxI = 736;
-
-X = zeros(maxJ,maxI);
-for i       = 1:maxJ
-    X(i,:) = X_array((i-1)*maxI+1:i*maxI);
-end
 
 %% Visualize
 customcolor = jet (5);
 figure()
-plot(TP006_time, TP006_w4,'Color',customcolor(1,:),'LineWidth',2)
+plot(timeseries006, TP006_w4,'Color',customcolor(1,:),'LineWidth',2)
 hold on
-plot(TP006_time, TP006_w6,'Color',customcolor(2,:),'LineWidth',2)
-plot(TP006_time, TP006_w8,'Color',customcolor(3,:),'LineWidth',2)
-plot(TP006_time, TP006_w12,'Color',customcolor(4,:),'LineWidth',2)
-plot(TP006_time, TP006_w16,'Color',customcolor(5,:),'LineWidth',2)
+plot(timeseries006, TP006_w6,'Color',customcolor(2,:),'LineWidth',2)
+plot(timeseries006, TP006_w8,'Color',customcolor(3,:),'LineWidth',2)
+plot(timeseries006, TP006_w12,'Color',customcolor(4,:),'LineWidth',2)
+plot(timeseries006, TP006_w16,'Color',customcolor(5,:),'LineWidth',2)
 legend('4 m', '6 m', '8 m', '12 m', ' 16 m','Location','northeast')
 legend 'Boxoff'
 xlabel('Time (cycles)')
