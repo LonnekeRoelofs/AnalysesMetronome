02f1653920dd9800b43643d11883f1f97059dfc4
LonnekeRoelofs <lonneke.roelofs@live.nl>
2020-10-02 10:23:44 +0200

    function works

--- src/CompareTPtime.m
+++ src/CompareTPtime.m
@@ -9,29 +9,6 @@ clc
 experiment  = 'Exp006';
 [TP006 timeseries006] = LoadingTPdata(experiment);
 
-cycle       = 'cycle13076';
-TP006_13076 = load(['..\data\processed\',experiment,'\',cycle,'\TidalPrism.ASC']);
-cycle       = 'cycle10915';
-TP006_10915 = load(['..\data\processed\',experiment,'\',cycle,'\TidalPrism.ASC']);
-cycle       = 'cycle08863';
-TP006_08863 = load(['..\data\processed\',experiment,'\',cycle,'\TidalPrism.ASC']);
-cycle       = 'cycle07869';
-TP006_07869 = load(['..\data\processed\',experiment,'\',cycle,'\TidalPrism.ASC']);
-cycle       = 'cycle06884';
-TP006_06884 = load(['..\data\processed\',experiment,'\',cycle,'\TidalPrism.ASC']);
-cycle       = 'cycle05887';
-TP006_05887 = load(['..\data\processed\',experiment,'\',cycle,'\TidalPrism.ASC']);
-cycle       = 'cycle04401';
-TP006_04401 = load(['..\data\processed\',experiment,'\',cycle,'\TidalPrism.ASC']);
-cycle       = 'cycle02281';
-TP006_02281 = load(['..\data\processed\',experiment,'\',cycle,'\TidalPrism.ASC']);
-cycle       = 'cycle00771';
-TP006_00771 = load(['..\data\processed\',experiment,'\',cycle,'\TidalPrism.ASC']);
-cycle       = 'cycle00291';
-TP006_00291 = load(['..\data\processed\',experiment,'\',cycle,'\TidalPrism.ASC']);
-
-% Making time vector for plotting
-TP006_time  = [291 771 2281 4401 5887 6884 7869 8863 10915 13076];
 % Finding tidal prism at different locations along flume for all time steps
 TP006_w4    = [TP006_00291(104) TP006_00771(104) TP006_02281(104) TP006_04401(104) TP006_05887(104) TP006_06884(104) TP006_07869(104) TP006_08863(104) TP006_10915(104) TP006_13076(104)];
 TP006_w6    = [TP006_00291(184) TP006_00771(184) TP006_02281(184) TP006_04401(184) TP006_05887(184) TP006_06884(184) TP006_07869(184) TP006_08863(184) TP006_10915(184) TP006_13076(184)];
@@ -40,7 +17,7 @@ TP006_w12   = [TP006_00291(424) TP006_00771(424) TP006_02281(424) TP006_04401(42
 TP006_w16   = [TP006_00291(584) TP006_00771(584) TP006_02281(584) TP006_04401(584) TP006_05887(584) TP006_06884(584) TP006_07869(584) TP006_08863(584) TP006_10915(584) TP006_13076(584)];
 
 %getting X matrix for plotting
-load(['..\data\processed\',experiment,'\',cycle,'\X_array.ASC']);
+load(['..\data\processed\',experiment,'\cycle',num2str(timeseries006(end)),'\X_array.ASC']);
 maxJ = 118;
 maxI = 736;
 
